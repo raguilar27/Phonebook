5bf472b891d28c6fc190b18f9db00ba1fa9a365c
Rogelio Aguilar <raguilar27@github.com>
2021-10-12 18:20:28 -0700

    displays persons and person by id

--- index.js
+++ index.js
@@ -24,9 +24,31 @@ const persons = [
   },
 ];
 
+app.get("/info", (req, res) => {
+  const people = persons.length;
+  const date = new Date();
+  const info = `<div>
+      <p>Phonebook has info for ${people} people</p>
+      <p>${date}</p>
+  </div>`;
+  res.send(info);
+});
+
 app.get("/api/persons", (req, res) => {
   res.send(persons);
 });
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.send(person);
+  } else {
+    res.send(404);
+  }
+});
+
+app.delete("/api/person/:id", (req, res) => {});
 
 const PORT = 3001;
 app.listen(PORT, () => {
